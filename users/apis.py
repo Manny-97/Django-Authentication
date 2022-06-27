9da1e70998aec8b47bb23b4ea87cb6c4e00a1224
Muhammed <muhammed@sendme.ng>
2022-06-27 07:52:30 +0100

    Login page Implemented

--- users/apis.py
+++ users/apis.py
@@ -1,13 +1,26 @@
+from click import password_option
 from django.shortcuts import render
 from rest_framework import views, exceptions, permissions, response
 
 from . import serializer as user_serializer
-
+from . import service
 class RegisterApi(views.APIView):
     def post(self, request):
         serializer = user_serializer.UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
+        serializer.instance = service.create_user(user_dc=data)
         print(data)
-        return response.Response(data={"hello": "Manny"})
+        return response.Response(data=serializer.data)
 
+class LoginApi(views.APIView):
+    def post(self, request):
+        email = request.data["email"]
+        password = request.data["password"]
+        user = service.user_email_selector(emai=email)
+# We don't want to tell the user which of the login details is actually wrong
+        if user is None:
+            raise exceptions.AuthenticationFailed("Invalid Credentials")
+
+        if not user.check_password(raw_password=password):
+            raise exceptions.AuthenticationFailed("Invalid Credentials")
--- users/service.py
+++ users/service.py
@@ -1,6 +1,10 @@
 import dataclasses
 from typing import TYPE_CHECKING
 
+from click import password_option
+from . import models
+from pytest import Instance
+
 if TYPE_CHECKING:
     from . import User
 @dataclasses.dataclass
@@ -14,8 +18,25 @@ class UserDataclass:
     @classmethod
     def from_instance(cls, users:"User") -> "UserDataclass":
         return cls(
-            name=user.name,
-            phone_number=user.phone_number,
-            email=user.email,
-            id=user.id
+            name=users.name,
+            phone_number=users.phone_number,
+            email=users.email,
+            id=users.id
         )
+# Create user
+def create_user(user_dc: "UserDataclass") -> "UserDataclass":
+    instance = models.User(
+        name=user_dc.name,
+        phone_number=user_dc.phone_number,
+        email=user_dc.email
+    )
+    if user_dc.password is not None:
+        instance.set_password(user_dc.password)
+
+    instance.save()
+    return UserDataclass.from_instance(instance)
+
+def user_email_selector(email: str) -> "User":
+    user = models.User.objects.filter(email=email).first()
+
+    return user
